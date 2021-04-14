4567887edb6593a961d947e8627b8dc90fb9674f
Kausinho <kausinhobrunow@gmail.com>
2021-04-14 20:02:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Landing Page</title>
+    <title>Starry.Code</title>
     <link href="css/style.css" rel="stylesheet" />
     <link rel="stylesheet" href="css/font-awesome.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
@@ -10,30 +10,35 @@
 </head>
 <body>
     <header>
-        <div class="container-single">
-        <div class="logo">LOGOMARCA</div><!--logo-->
+    <div class="container-single">
+    <div class="logo"><a href="">Starry.Code</a></div><!--logo-->
             <nav class="desktop">
                 <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Sobre</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Contato</a></li>
+                    <li><a href="home">Home</a></li>
+                    <li><a href="sobre">Sobre</a></li>
+                    <li><a href="contato">Contato</a></li>
                 </ul>
             </nav>
         </div><!--container-->
     </header>
 
-    <section class="texto-principal">
-        <div class="container">
-            <div class="texto-central">
-                <h2>Meu título do meu super texto</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet ex quis mauris vehicula dignissim eget id elit. Cras consequat eleifend metus vitae faucibus. Morbi sit amet facilisis quam. Etiam tincidunt imperdiet lectus sit amet luctus. Sed faucibus lobortis nisl, sed fermentum ligula ullamcorper at. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur ullamcorper elit non risus laoreet, sit amet venenatis sapien pharetra. Pellentesque ac rutrum dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis lacinia elementum fringilla. Etiam finibus, ante in sagittis scelerisque, diam nisl vehicula orci, sed semper lorem arcu id velit. Etiam volutpat nisi ac justo fringilla, id vulputate metus pharetra. Nam vulputate eleifend mattis. Phasellus pretium eros vel ante tempor sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                Nam dapibus, nisl tristique rhoncus dictum, mauris neque efficitur diam, quis auctor massa massa sed leo. Quisque sed ante in tellus fringilla posuere at ac nunc. Nam eget pulvinar lorem. Morbi at nunc vitae dui rutrum sagittis ac nec libero. Phasellus accumsan metus ac pellentesque egestas. Morbi lacus leo, bibendum in mattis a, dignissim sed dui. Integer et arcu elit.</p>
-            </div><!--texto-central-->
-            <div class="btn-blog">
-                <a href=""><i class="fa fa-angle-down"></i></a>
-            </div><!--btn-blog-->
-        </div><!--container-->
-    </section><!--texto-principal-->
+    <?php
+    if(isset($_GET['url'])){
+        if(file_exists($_GET['url']).'.html'){
+            include($_GET['url'].'.html');
+        }else{
+            include('404.html');
+        }
+    }else{
+        include('home.html');
+    }
+
+?>
+
+    
+
+    <footer>
+        <h2>Todos os direitos reservados!</h2>
+    </footer>
 </body>
 </html>
