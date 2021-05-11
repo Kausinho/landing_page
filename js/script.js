c10d3aa2bdaad0ed9e550b7177fe48e95c9dd4f7
Kausinho <kausinhobrunow@gmail.com>
2021-05-11 10:31:20 -0300

    Mudança menu mobile
    Mudança no modo de execução do menu mobile.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-$(function (){
-    $('.btn-menu-open').on('click', function(){
-        $('.btn-menu-open').css('display','none');
+$(function(){
+    $('.btn-menu-open').click(function(){
+        $('#mobile').slideToggle('slow',);
+        $('.btn-menu-open').fadeOut('slow');
         $('.btn-menu-close').fadeIn('slow');
-        $('.mobile ul').fadeIn('slow');
     });
 
-    $('.btn-menu-close').on('click', function(){
+    $('.btn-menu-close').click(function(){
+        $('#mobile').slideToggle('slow');
         $('.btn-menu-open').fadeIn('slow');
-        $('.btn-menu-close').css('display','none');
-        $('btn-mneu-close').fadeOut('slow');
-        $('.mobile ul').fadeOut('slow');
+        $('.btn-menu-close').fadeOut('slow');
     });
 });
