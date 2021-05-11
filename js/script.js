215c91e10407440d2321a074b9e56f22291729ca
Kausinho <kausinhobrunow@gmail.com>
2021-05-11 10:58:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(function(){
     $('.btn-menu-open').click(function(){
         $('#mobile').slideToggle('slow',);
-        $('.btn-menu-open').fadeOut('slow');
-        $('.btn-menu-close').fadeIn('slow');
+        $('.btn-menu-open').fadeToggle('slow','linear');
+        $('.btn-menu-close').fadeToggle('slow','linear');
     });
 
     $('.btn-menu-close').click(function(){
         $('#mobile').slideToggle('slow');
-        $('.btn-menu-open').fadeIn('slow');
-        $('.btn-menu-close').fadeOut('slow');
+        $('.btn-menu-open').fadeToggle('slow','linear');
+        $('.btn-menu-close').fadeToggle('slow','linear');
     });
 });
